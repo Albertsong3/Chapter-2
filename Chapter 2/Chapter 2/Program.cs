7a586241c47685bf08b02020ceca4422ae0301ea
Albertsong3 <gregalbertson@rocketmail.com>
2014-09-08 08:39:22 -0400

    random coding

---
+++ Chapter 2/Chapter 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The floor space is {0},area"
+            int length = 15;
+            int width = 25;
+            length * width = area;
+        }
+    }
+}
