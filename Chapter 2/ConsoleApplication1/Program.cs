ffc0ca0e38eb23bc5b6a7484c3d5d8fb30252b7b
Albertsong3 <gregalbertson@rocketmail.com>
2014-09-10 08:39:16 -0400

    up to #16

---
+++ Chapter 2/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chicken1 = 23;
+            int chicken2 = 27;
+            int chicken3 = 25;  
+            int chicken4 = 20; 
+
+            int sum = chicken1 + chicken2 + chicken3 + chicken4; 
+            int dozen = sum;
+            int dozen2 = sum / 12;
+            Console.WriteLine("The chickens laid a total of {0} eggs, which is {1} dozen and 11 eggs", dozen, dozen2);
+        }
+    }
+}
